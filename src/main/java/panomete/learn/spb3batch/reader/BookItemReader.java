b4d0e1c175bec8628091e0527c943afae17116bf
oat431 <oat431@gmail.com>
2023-08-26 02:53:21 +0700

    🔨: optimized import

--- src/main/java/panomete/learn/spb3batch/reader/BookItemReader.java
+++ src/main/java/panomete/learn/spb3batch/reader/BookItemReader.java
@@ -1,12 +1,9 @@
 package panomete.learn.spb3batch.reader;
 
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
 import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import panomete.learn.spb3batch.entity.Books;
